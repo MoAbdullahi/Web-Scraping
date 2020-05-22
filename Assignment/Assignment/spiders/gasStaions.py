55151c6f270cb2edecffa06c641cf4c048622458
hashimkey <insham53@hotmail.com>
2020-05-22 10:20:51 +0200

    unnecessary libraries removed

--- Assignment/Assignment/spiders/gasStaions.py
+++ Assignment/Assignment/spiders/gasStaions.py
@@ -2,9 +2,6 @@ import scrapy
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-
 
 class Stations(scrapy.Spider):
     name = "station"
