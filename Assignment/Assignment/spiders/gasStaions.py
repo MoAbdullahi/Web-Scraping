67a2f46059c2f33786b9fc4c47d57e4cb5973db1
hashimkey <insham53@hotmail.com>
2020-05-22 10:06:22 +0200

    first commit
    The Assignment is done in two different ways:
    1-Notebook with only selenium
    2-scrapy and selenium in Scripy project

---
+++ Assignment/Assignment/spiders/gasStaions.py
@@ -0,0 +1,30 @@
+import scrapy
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+class Stations(scrapy.Spider):
+    name = "station"
+    start_urls = [
+        'https://www.brandstof-zoeker.nl/',
+    ]
+
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.wait = WebDriverWait(self.driver, 10)
+
+    def parse(self, response):
+        self.driver.get(response.url)
+        while True:
+            data = self.driver.find_elements_by_xpath('//li[starts-with(@id, "station")]/ul/li[1]')
+            num_page_items = len(data)
+            for i in range(num_page_items):
+                address = data[i].text.split(",")[0]
+                city    = data[i].text.split(",")[1]
+                yield {"address": address, "city":city}
+            try:
+                response = response.replace(body=self.driver.page_source)
+            except TimeoutException:break
